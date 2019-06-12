8d1fa9f6253926fad616f9f55d17b6a86d2c4d5e
Jared Wilkens <jared.wilken@appfolio.com>
2019-06-12 10:34:33 -0700

    JW - add contoller for images that includes proper create and show actions

---
+++ app/controllers/images_controller.rb
@@ -0,0 +1,28 @@
+class ImagesController < ApplicationController
+  def index
+    @image = Image.all
+  end
+
+  def show
+    @image = Image.find(params[:id])
+  end
+
+
+  def new
+    @image = Image.new
+  end
+
+  def create
+    img_params = params.require(:image).permit(:name, :url)
+    @image = Image.new(img_params)
+
+    if @image.save
+      render :show
+      flash[:success] = "successfully added image"
+    else
+      render :new
+    end
+
+  end
+
+end
