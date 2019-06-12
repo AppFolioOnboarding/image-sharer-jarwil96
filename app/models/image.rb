320e171a3ff5f4d19572f573658608d77baf1abf
Jared Wilkens <jared.wilken@appfolio.com>
2019-06-12 10:46:29 -0700

    JW - add support files and style files for this branch

---
+++ app/models/image.rb
@@ -0,0 +1,6 @@
+class Image < ApplicationRecord
+  validates :name, :url, presence: true, uniqueness: true
+  URL_FORMAT = /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/ix
+  validates :url, format: { with: URL_FORMAT, :multiline => true, message: 'Your url is not valid' }
+
+end
