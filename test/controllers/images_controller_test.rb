4b90c7d8a0905454e990e6564d12c56d2bafda0c
Jared Wilkens <jared.wilken@appfolio.com>
2019-06-12 15:36:10 -0700

    JW - fix format issues

--- test/controllers/images_controller_test.rb
+++ test/controllers/images_controller_test.rb
@@ -2,15 +2,14 @@ require 'test_helper'
 
 class ImagesControllerTest < ActionDispatch::IntegrationTest
   setup do
-   @image = images(:one)
+    @image = images(:one)
   end
-
 
   test 'should get show' do
     get image_url(@image)
     assert_response :success
   end
-  
+
   test 'should get new' do
     get new_image_url
     assert_response :success
@@ -20,6 +19,4 @@ class ImagesControllerTest < ActionDispatch::IntegrationTest
     get '/'
     assert_response :success
   end
-
-
 end
