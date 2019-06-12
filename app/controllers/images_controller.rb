48e432f04ec1dfd61659cf890c8e7952c5abbf71
Jared Wilkens <jared.wilken@appfolio.com>
2019-06-12 10:44:35 -0700

    JW - add views for all actions to render images

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -7,7 +7,6 @@ class ImagesController < ApplicationController
     @image = Image.find(params[:id])
   end
 
-
   def new
     @image = Image.new
   end
@@ -15,14 +14,12 @@ class ImagesController < ApplicationController
   def create
     img_params = params.require(:image).permit(:name, :url)
     @image = Image.new(img_params)
-
     if @image.save
       render :show
       flash[:success] = "successfully added image"
     else
       render :new
     end
-
   end
 
 end
--- test/controllers/images_controller_test.rb
+++ test/controllers/images_controller_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class ImagesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+ def setup
+   @image = Image.new(name: 'test', url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCcvZuJfs9XIzDk3HwMxuFlavuvOrGHCp_v7sSuiMIo6iBeKiO' )
+ end
+
 end
