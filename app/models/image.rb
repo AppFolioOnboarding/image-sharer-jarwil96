29dd1c69579854a4d4f11f7be344343a055905b7
Jared Wilkens <jared.wilken@appfolio.com>
2019-06-13 13:25:30 -0700

    JW - add act_as_taggable into database schema

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,4 +2,5 @@ class Image < ApplicationRecord
   validates :name, :url, presence: true, uniqueness: true
   URL_FORMAT = /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/ix
   validates :url, format: { with: URL_FORMAT, multiline: true, message: 'Your url is not valid' }
+  acts_as_taggable
 end
