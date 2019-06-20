62836f969c3eb4fd3d4da27f79de8f907d69e562
Jared Wilkens <jared.wilken@appfolio.com>
2019-06-20 16:24:10 -0700

    JW - add footer and coresponding footer tests

--- app/javascript/image_sharing/components/App.js
+++ app/javascript/image_sharing/components/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import { inject } from 'mobx-react';
 import Header from './Header';
-import Footer from "./Footer";
-
+import Footer from './Footer';
+/* Put your components here: Flash Message, Form, Footer */
 class App extends Component {
   /* Add Prop Types check*/
   render() {
     return (
       <div>
         <Header title={'Tell us what you think'} />
-        /* Put your components here: Flash Message, Form, Footer */
+        <Footer />
       </div>
     )
   }
--- app/javascript/image_sharing/components/Footer.js
+++ app/javascript/image_sharing/components/Footer.js
@@ -2,6 +2,28 @@ import React, { Component } from 'react';
 
 class Footer extends Component {
   /* Implement your Footer component here */
+  render() {
+    var message = 'Copyright: AppFolio Inc. Onboarding';
+    const pStyle = {
+      fontSize: '10px',
+      textAlign: 'center'
+    };
+
+
+    return (
+      <body>
+      <div>
+      <footer >
+      <p style={pStyle}>{message}</p>
+      </footer>
+        </div>
+      </body>
+    );
+  }
 }
+
+
+
+
 
 export default Footer;
